5bdcecfd107a484b800d3da12dd7f38445bd428c
Imad Mohamad <yehya.mh@live.com>
2024-04-20 13:43:49 +0200

    Add Exercise 2

---
+++ Exercise 2.py
@@ -0,0 +1,7 @@
+first_string = "I am unwritten, "
+second_string = "can’t read my mind, "
+third_string = "I’m undefined."
+
+print(f"{first_string} {second_string} {third_string}")
+print(first_string +" "+ second_string+" "+third_string)
+print("{0} {1} {2}".format(first_string,second_string, third_string))
